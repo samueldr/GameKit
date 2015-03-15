9d5ab60b7b1db606cde4e153c9368883dc5170d9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-03-15 17:45:02 -0400

    Added loader function for texture and sound.

--- ResourcesLoader.lua
+++ ResourcesLoader.lua
@@ -15,8 +15,30 @@ function Resources.xml(name)
 	BaconBox.ResourceManager_loadFlashExporterXML(resources_path  .. "/" .. name .. ".xml")
 end
 
+-- Loads a file as a texture. When given no key, guesses the key from the filename.
+-- The guess is made by stripping everything from and including the last dot
+-- mypicture.png -> mypicture
+function Resources.texture(file, key)
+	assert(file, "No filename given to Resources.texture().")
+	if not key then
+		key = file:gsub("%.%w+$", "")
+	end
+	BaconBox.ResourceManager_loadTexture(key, resources_path .. "/" .. file)
+end
+
 function Resources.font(name, file, ...)
 	BaconBox.ResourceManager_loadFont(name, resources_path .. "/" .. file, ...)
+end
+
+-- Loads a file as a sound. When given no key, guesses the key from the filename.
+-- The guess is made by stripping everything from and including the last dot
+-- mysound.wav -> mysound
+function Resources.sound(file, key)
+	assert(file, "No filename given to Resources.texture().")
+	if not key then
+		key = file:gsub("%.%w+$", "")
+	end
+	BaconBox.ResourceManager_loadSoundRelativePath(key, file);
 end
 
 return Resources
