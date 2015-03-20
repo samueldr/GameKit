e51a67531ddde6a785fbc2c82a64548e0c499477
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-03-19 22:09:54 -0400

    Added MainWindow shim

---
+++ MainWindow.lua
@@ -0,0 +1,7 @@
+-- Thin sugarcoat around BaconBox MainWindow
+
+local _main_window = BaconBox.Engine_getMainWindow()
+
+-- TODO : If needed, to add methods, add a metatable and stuff...
+
+return _main_window
