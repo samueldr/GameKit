3608e8703ca80286e592b7c5a1cc9fcdb9fc37f2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-04-07 13:07:02 -0400

    Added GameKit.Utils.string_split().

--- Utils.lua
+++ Utils.lua
@@ -9,4 +9,32 @@ function Utils.center_entity(entity)
 	entity:setPosition(context/2)
 end
 
+-- Local references.
+-- Used in the pilfered code, don't know how much it helps though...
+local strfind = string.find
+local strsub  = string.sub
+local tinsert = table.insert
+-- Split text into a list consisting of the strings in text,
+-- separated by strings matching delimiter (which may be a pattern). 
+-- example: string_split("Anna, Bob, Charlie,Dolores", ",%s*")
+-- http://lua-users.org/wiki/SplitJoin
+function Utils.string_split(text, delimiter)
+	local list = {}
+	local pos = 1
+	if strfind("", delimiter, 1) then -- this would result in endless loops
+		error("delimiter matches empty string!")
+	end
+	while 1 do
+		local first, last = strfind(text, delimiter, pos)
+		if first then -- found?
+			tinsert(list, strsub(text, pos, first-1))
+			pos = last+1
+		else
+			tinsert(list, strsub(text, pos))
+			break
+		end
+	end
+	return list
+end
+
 return Utils
